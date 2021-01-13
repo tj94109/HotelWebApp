d9459f0c3ed0b8e04352516d43a7de0493861c4c
tj94109 <52139368+tj94109@users.noreply.github.com>
2021-01-13 12:30:49 -0800

    used

--- src/main/java/com/terrance/HotelWebAppApplication.java
+++ src/main/java/com/terrance/HotelWebAppApplication.java
@@ -2,12 +2,17 @@ package com.terrance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HotelWebAppApplication {
+public class HotelWebAppApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelWebAppApplication.class, args);
 	}
-
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HotelWebAppApplication.class);
+	}
 }
